cc791f1123f131962f4d16f81cdd9d92f9a7311f
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-05-14 18:39:46 -0300

    refactor: Update the cody base of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from './components/Header'
 import { About } from './sections/About'
 import { Home } from './sections/Home'
+import { Qualifications } from './sections/Qualifications'
 import { Service } from './sections/Service'
 import { Skill } from './sections/Skill'
 
@@ -15,6 +16,7 @@ function App() {
         <About/>
         <Skill/>
         <Service/>
+        <Qualifications/>
       </main>
     </>
   )
