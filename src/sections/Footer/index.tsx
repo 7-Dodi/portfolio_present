b780d2ce88f8fad66f344b4173383d7f250bc2ce
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-05-16 21:09:50 -0300

    created: Created the sections footer

---
+++ src/sections/Footer/index.tsx
@@ -0,0 +1,41 @@
+import "./style.css";
+
+export const Footer = () => {
+    return(
+        <footer className="footer">
+            <div className="conatinerFooter container">
+                <h1 className="footerTitle">Douglas Silva.</h1>
+
+                <ul className="footerList">
+                    <li><a href="#about" className="footerLink">Sobre mim</a></li>
+                    <li><a href="#portfolio" className="footerLink">Projetos</a></li>
+                    <li><a href="#certificate" className="footerLink">Certificados</a></li>
+                </ul>
+
+                <div className="footerSocial">
+                    <div title="Instagram">
+                        <a href="https://www.instagram.com/7_douglassilva" className="homeSocial-Icon" target="_blank">
+                        <i className="uil uil-instagram-alt footerSocial-icon"></i>
+                        </a>
+                    </div>
+
+                    <div title="Github">
+                        <a href="https://github.com/7-Dodi" className="homeSocial-Icon" target="_blank">
+                        <i className="uil uil-github footerSocial-icon"></i>
+                        </a>
+                    </div>
+
+                    <div title="Linkedin">
+                        <a href="https://www.linkedin.com/in/douglas-silva-araújo-ba3336204/" className="homeSocial-Icon" target="_blank">
+                        <i className="uil uil-linkedin footerSocial-icon"></i>
+                        </a>
+                    </div>
+                </div>
+
+                <span className="footer-copy">
+                    &#169; Douglas Silva. All rigths reserved.
+                </span>
+            </div>
+        </footer>
+    )
+}
