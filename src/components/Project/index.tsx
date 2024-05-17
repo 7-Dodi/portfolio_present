c2241f024848f82b5abeb3ba496dd1924e086dc9
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-05-16 21:11:33 -0300

    created: Created the component project

---
+++ src/components/Project/index.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import "./style.css";
+
+interface Props {
+    image?: string,
+    title?: string,
+    description?: string,
+    info?: string[],
+    link?: string
+}
+
+export const Project:React.FC<Props> = ({image, title, description, link, info}) => {
+    const [toggleStateProject, setToggleStateProject] = useState(false);
+
+    const handleSetStateProject = (value: boolean) => {
+        setToggleStateProject(value);
+    }
+    
+    return(
+        <div className="portifolioProjeto">
+            <img className="projetoImage" src={image} alt="Imagem do projeto"/>
+            <h3 className="projetoTitle">{title}</h3>
+
+            <span className="projetoButton" onClick={()=>handleSetStateProject(true)}>View More
+                <i className="uil uil-arrow-right projeto__button-icon"></i>
+            </span>
+
+            <div className={toggleStateProject === true ? "projetoModal projetoModal-active" : "projetoModal"}>
+                <div className="projetoModal-content">
+                    <i className="uil uil-times projetoModal-close" onClick={()=>handleSetStateProject(false)}></i>
+                    <h3 className="projetoModal-title">{title}</h3>
+                    <p className="projetoModel-description">{description}</p>
+
+                    <ul className="projetoModal-projetos grid">
+                        {info && info.map((infoItem, index) => (
+                            <li key={index} className="projetoModal-projeto">
+                                <i className="uil uil-check-circle projetoModal-icon"></i>
+                                <p className="projetoModal-info">{infoItem}</p>
+                            </li>
+                        ))}
+                        <li className="projetoModal-projeto">
+                                <i className="uil uil-github-alt projetoModal-icon"></i>
+                                <p className="projetoModal-info projetoModal-link">Link do projeto:  <a href={link} target="_blank">{link}</a></p>
+                            </li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    )
+}
