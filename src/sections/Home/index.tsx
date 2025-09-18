3db41d2517b5642956f900711c8c5e09c7b38ba7
7-Dodi <douglassilvaaraujo78@gmail.com>
2025-09-18 09:22:52 -0300

    refactor: :wrench: Atualizando instagram

--- src/sections/Home/index.tsx
+++ src/sections/Home/index.tsx
@@ -4,29 +4,44 @@ import scroll from "../../assets/scroll.svg";
 import "./style.css";
 
 export const Home = () => {
-    return(
-        <section className="home section" id="home">
-            <div className="homeContainer container grid">
-                <div className="homeContent grid">
-                    <div className="homeSocial">
-                        <Social title="Instagram" link="https://www.instagram.com/7_douglassilva" name="instagram" key="instagam"/>
-                        <Social title="Github" link="https://github.com/7-Dodi" name="github-alt" key="github-alt"/>
-                        <Social title="Linkedin" link="https://www.linkedin.com/in/douglas-silva-araújo-ba3336204/" name="linkedin-alt" key="linkedin-alt"/>
-                    </div>
+  return (
+    <section className="home section" id="home">
+      <div className="homeContainer container grid">
+        <div className="homeContent grid">
+          <div className="homeSocial">
+            <Social
+              title="Instagram"
+              link="https://www.instagram.com/7_dood.dev"
+              name="instagram"
+              key="instagam"
+            />
+            <Social
+              title="Github"
+              link="https://github.com/7-Dodi"
+              name="github-alt"
+              key="github-alt"
+            />
+            <Social
+              title="Linkedin"
+              link="https://www.linkedin.com/in/douglas-silva-araújo-ba3336204/"
+              name="linkedin-alt"
+              key="linkedin-alt"
+            />
+          </div>
 
-                    <div className="homeImg"></div>
+          <div className="homeImg"></div>
 
-                    <Data/>       
-                </div>
+          <Data />
+        </div>
 
-                <div className="home-scroll">
-                    <a href="#about" className="home__scroll-button button--flex">
-                        <img src={scroll} alt="svg" />
-                        <span className="home__scroll-name">Scroll Down</span>
-                        <i className="uil uil-arrow-down home__scroll-arrow"></i>
-                    </a>
-                </div>
-            </div>
-        </section>
-    )
-}
+        <div className="home-scroll">
+          <a href="#about" className="home__scroll-button button--flex">
+            <img src={scroll} alt="svg" />
+            <span className="home__scroll-name">Scroll Down</span>
+            <i className="uil uil-arrow-down home__scroll-arrow"></i>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
